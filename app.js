be5fe86ea573c675bee102039288908124702fe7
marianosim <mariano.simantob@gmail.com>
2021-09-02 21:46:47 -0300

    Add new modules

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const methodOverride =  require('method-override'); // Métodos PUT y DELETE
 const routesMain = require("./routers/main")
 app.use(express.static('public'));
 app.set("view engine", "ejs");
@@ -7,5 +8,8 @@ app.set("view engine", "ejs");
 
 app.listen(process.env.PORT || 3000,()=>{console.log("server iniciado")});
 app.use("/",routesMain);
+app.use(methodOverride('_method')); // pisa el method="POST" en el formulario por PUT y DELETE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
