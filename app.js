848140621546b6fd503872dc9de6c0a07e2e9b52
marianosim <mariano.simantob@gmail.com>
2021-09-02 23:15:42 -0300

    productController, routes y app.js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const methodOverride =  require('method-override'); // Métodos PUT y DELETE
 const routesMain = require("./routers/main")
+const routesProduct = require('./routers/products')
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 
 app.listen(process.env.PORT || 3000,()=>{console.log("server iniciado")});
 app.use("/",routesMain);
-app.use(methodOverride('_method')); // pisa el method="POST" en el formulario por PUT y DELETE
+app.use("/product",routesProduct);
+app.use(methodOverride('_method')); 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
---
+++ routers/products.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const mainController = require("../controllers/productsController");
+
+module.exports = router;
