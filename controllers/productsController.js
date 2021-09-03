5a56caa193edf3d87579d01bc794cafe505de6af
marianosim <mariano.simantob@gmail.com>
2021-09-02 21:14:41 -0300

    Add productsController.js

---
+++ controllers/productsController.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		res.render('products', {productos : products}) 
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		idParams = req.params.id
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		let productoAMostrar = products.find(n => n.id == idParams);
+		res.render('product', {detalleProducto : productoAMostrar})
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('new-product')
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		let newProduct = {
+			id: Date.now(),
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			image: req.file.filename,
+		};
+		
+		products.push(newProduct);
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+
+		res.redirect('/products')
+		
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		let idProduct = req.params.id;
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		let productToEdit = products.filter(n => n.id == idProduct);
+		res.render('product-edit-form', {productToEdit : productToEdit})
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		let idParams = req.params.id;
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		products.forEach(product => {
+			if (product.id == idParams){
+				product.name = req.body.name;
+				product.price = req.body.price;
+				product.discount = req.body.discount;
+				product.category = req.body.category;
+				product.description = req.body.description;
+
+			};
+		})
+		fs.writeFileSync('productsFilePath', JSON.stringify(products));
+		res.redirect('/products')
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		let idProduct = req.params.id;
+		let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		let updatedProducts = products.filter(n => n.id != idProduct);
+		fs.writeFileSync('productsFilePath', JSON.stringify(products));
+		res.redirect('/products')
+
+	}
+};
+
+module.exports = controller;
