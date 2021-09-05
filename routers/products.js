c7d8c1d0f4cd62cb7295d83f8f38c813e5d362ca
marianosim <mariano.simantob@gmail.com>
2021-09-05 13:37:59 -0300

    Add multer, create product, store product

--- routers/products.js
+++ routers/products.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/productsController");
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+    let folder = path.join(__dirname, '../public/images/products');
+    callback(null, folder)
+    },
+    filename: (req, file, callback) => {
+    let imageName = Date.now() + path.extname(file.originalname);
+    callback(null, imageName);
+    }
+    });
+    const fileUpload = multer({storage})
+
 
 /*** GET ALL PRODUCTS ***/ 
 router.get('/products', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
-router.get('/products/create', productsController.create); 
-router.post('/products', productsController.store); 
+router.get('/create', productsController.create); 
+router.post('/', fileUpload.any('product-image'), productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
