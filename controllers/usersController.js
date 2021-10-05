671156be19440e3d9c2a2ba77d5507f03fc03d18
marianosim <mariano.simantob@gmail.com>
2021-10-05 12:59:25 -0300

    agrego campo imagen de usuario vista-controlador-ruta

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+const usersFilePath = path.join(__dirname, '../data/users.json');
 
 const controller = {
     userhome: (req, res)=>{
@@ -10,7 +10,26 @@ const controller = {
         res.render("login")
     },
     register:(req,res)=>{
-        res.render("register")},
+        res.render("register")
+    },
+    new: (req, res) => {
+        let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+		console.log(req.file)
+		let newUser = {
+			id: Date.now(),
+			name: req.body.nombre,
+			userName: req.body.nick,
+			dni: req.body.dni,
+            password: req.body.pass,
+            email: req.body.email,
+			image: req.file.filename
+				
+		};
+        users.push(newUser);
+		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+
+		res.redirect('/users')
+    }
 };
 
 module.exports = controller;
--- routers/users.js
+++ routers/users.js
@@ -4,18 +4,19 @@ const usersController = require("../controllers/usersController");
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-    let folder = path.join(__dirname, '../public/images/products');
+    let folder = path.join(__dirname, '../public/images/users');
     callback(null, folder)
     },
     filename: (req, file, callback) => {
-    let imageName = Date.now() + path.extname(file.originalname);
-    callback(null, imageName);
+    let userImageName = Date.now() + path.extname(file.originalname);
+    callback(null, userImageName);
     }
     });
     const fileUpload = multer({storage:storage})
 
 router.get('/', usersController.userhome);
 router.get('/login', usersController.login);
-router.get("/register",fileUpload.single('user-image'), usersController.register);
+router.get("/register", usersController.register);
+router.post("/",fileUpload.single('user-image'), usersController.new);
 
 module.exports = router;
