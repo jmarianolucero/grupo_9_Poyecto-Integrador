dac19aedca8552081d8a76cf1fb59181a388862e
marianosim <mariano.simantob@gmail.com>
2021-11-09 17:04:19 -0300

    Diseño view Home

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,18 @@
+const db = require('../src/database/models');
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+const Products = db.Product;
+const Category = db.Category;
+const Color = db.Color;
+
 const controller = {
     "home":(req,res)=>{
-        res.render("home")
+        Products.findAll({
+			include: ['categories']
+		})
+			.then(products => {
+				res.render('home', { products })
+			})
     },
     "cart":(req,res)=>{
         res.render("shopping-cart")
