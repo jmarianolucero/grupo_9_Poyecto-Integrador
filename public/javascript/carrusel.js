33e2eac94074652f6b108b39eccec42aa8cbf031
primus <andressueyro@gmail.com>
2021-11-30 00:59:23 -0300

    carruselmk2

--- public/javascript/carrusel.js
+++ public/javascript/carrusel.js
@@ -1,6 +1,6 @@
-windows.addEventListener("load", ()=>{
+addEventListener("DOMContentLoaded", ()=>{
 
-    const imagenes = [1,2,3,4,5,6,7,8]
+    const imagenes = ["/images/drums.png","/images/guitarra.png","/images/saxo.png"]
     let i = 1
     const img1 = document.querySelector("#img1")
     const img2 = document.querySelector("#img2")
@@ -16,6 +16,34 @@ windows.addEventListener("load", ()=>{
         divIndicadores.appendChild(div)
     }
 
+    progressbar.style.width= `${porcentaje_base}%`
+    img1.src = imagenes[0]
+    const circulos = document.querySelectorAll(".circles")
+    circulos[0].classList.add("resaltado")
+
+    const slideshow = () =>{
+        img2.src = imagenes[i]
+        const circulo_actual = Array.from(circulos).find(el=>el.id==i)
+        Array.from(circulos).forEach(cir=>cir.classList.remove("resaltado"))
+        circulo_actual.classList.add("resaltado")
+
+        img2.classList.add("active")
+        i++
+        porcentaje_actual+=porcentaje_base
+        progressbar.style.width=`${porcentaje_actual}%`
+        if(i==imagenes.length){
+            i=0
+            porcentaje_actual=porcentaje_base - porcentaje_base
+
+        }
+
+        setTimeout(() => {
+            img1.src=img2.src
+            img2.classList.remove("active")
+        }, 1000);
+    }
+
+    setInterval(slideshow,4000)
 })
 
 
